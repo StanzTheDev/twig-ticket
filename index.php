b507bf1fc18079db7d8338b8dc61a919dcbe6df7
StanzTheDev <135062697+StanzTheDev@users.noreply.github.com>
2025-10-29 07:54:41 +0100

    Add Twig and templates folder checks in index.php
    Added checks for Twig library and templates folder, displaying relevant messages.

--- index.php
+++ index.php
@@ -2,32 +2,23 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__ . '/lib/autoload.php';
+echo "PHP is working!<br>";
+echo "Current directory: " . __DIR__ . "<br>";
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
-$twig = new \Twig\Environment($loader);
 
-$assetFunction = new \Twig\TwigFunction('asset', function ($path) {
-    return '/' . ltrim($path, '/');
-});
-$twig->addFunction($assetFunction);
-
-$page = $_GET['page'] ?? 'landing';
-
-switch($page) {
-    case 'signup':
-        echo $twig->render('signup.twig');
-        break;
-    case 'login':
-        echo $twig->render('login.twig');
-        break;
-    case 'dashboard':
-        echo $twig->render('dashboard.twig');
-        break;
-    case 'ticket':
-        echo $twig->render('ticket.twig');
-        break;
-    default:
-        echo $twig->render('landing.twig');
-        break;
+if (is_dir(__DIR__ . '/lib/Twig')) {
+    echo "Twig library found!<br>";
+} else {
+    echo "ERROR: Twig library NOT found!<br>";
 }
+
+
+if (is_dir(__DIR__ . '/templates')) {
+    echo "Templates folder found!<br>";
+    $files = scandir(__DIR__ . '/templates');
+    echo "Template files: " . implode(', ', $files) . "<br>";
+} else {
+    echo "ERROR: Templates folder NOT found!<br>";
+}
+
+echo "<br>If you see this, the server is responding!";
